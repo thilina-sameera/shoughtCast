5fa8516b36012198a5992d99609b0c07bc32f1a5
abey <thilina.sameera92@gmail.com>
2015-06-09 09:36:37 +0530

    added libraries and designed structure

---
+++ home.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+
+		<link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
+
+		<script src="bootstrap/js/bootstrap.min.js"></script>
+		
+		<link rel="stylesheet" href="style/style.css" />
+		
+		<!--end of import css and js files-->
+		
+		<title></title>		
+	</head>
+	
+	<body>
+		
+		<div class="container-fluid">
+  			<div class="row">
+  				
+  			</div>
+  			
+  			<div class="row">
+  				<!--for main page-->
+  				<div class="col-md-8">
+  					
+  					<!--recorder goes here-->
+  					<div class="row">
+  						
+  					</div>
+  					
+  					<!--shought goes here-->
+  					<div class="row">
+  						
+  					</div>
+  				</div>
+  				
+  				<!--for side page-->
+  				<div class="col-md-4">
+  					<!--states goes here-->
+  					<div class="row">
+  						
+  					</div>
+  					
+  					<!--friends grid goes here-->
+  					<div class="row">
+  						
+  					</div>
+  				</div>
+  			</div>
+		</div>
+		
+		
+		
+	</body>
+</html>
